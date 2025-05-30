95fa89e17584ad87eef0219b07e747541bc78a06
Mohsen Maleki Fard <163771330+MohsenMalekiFard@users.noreply.github.com>
2025-05-30 09:03:06 +0330

    Add main

---
+++ main.py
@@ -0,0 +1,26 @@
+print('a)Run G-one Androin')
+print('b)Run G-one Windows')
+print('c)Run G-one Windows2')
+print('d)Run Machine Learning')
+print('e)Run G-one orginall')
+
+
+com = input('Enter:')
+
+if "a" in com:
+	import Android
+ 
+ 
+if "b" in com:
+	import windows
+ 
+ 
+if "c" in com:
+	import windows2
+ 
+ 
+if "d" in com:
+    import Learn
+    
+if "e" in com:
+    import virtual  
